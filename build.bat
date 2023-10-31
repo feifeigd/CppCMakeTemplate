a6997ffe9e97e30256a8c10160a5aa4a210bff6a
feifeigd <feifeigd@21cn.com>
2023-10-31 14:12:45 +0800

    cwd

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
 
 :: 生成sln
+::cmake --preset <configurePreset-name>
 cmake --preset=win-x64-debug
+
 :: 构建
+::cmake --build --preset <buildPreset-name> 
 cmake --build --preset=win-x64-debug
--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 
+#ifdef WIN32
+#include <direct.h>
+#define _getcwd getcwd
+#else
+#include <unistd.h>
+#endif // WIN32
+
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Hello World!" << endl;
+int main(int argc, char const* argv[]) {
+	char buffer[256];
+	
+    cout << "Hello World! cwd=" << getcwd(buffer, sizeof buffer - 1) << endl;
+	for (int i = 0; i < argc; i++)
+	{
+		cout << "i=" << i << ", arg=" << argv[i] << endl;
+	}
     return 0;
 }
-
